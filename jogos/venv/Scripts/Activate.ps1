86e0885cb89d871f3bfa1bfa0555949cdca726b7
joutav <jvo.maga@gmail.com>
2020-08-26 15:51:19 -0300

    atualiazndo venv

--- jogos/venv/Scripts/Activate.ps1
+++ jogos/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="F:\Minhas aulas\python\jogos\venv"
+$env:VIRTUAL_ENV="F:\Minhas aulas\python\aulas-python\jogos\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
