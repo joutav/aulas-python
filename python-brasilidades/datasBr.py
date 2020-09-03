1218c0b7d1fc677946151646774d5e33f9cb326c
joutav <jvo.maga@gmail.com>
2020-09-03 17:58:49 -0300

    Implementacao de requets, APIs.

--- python-brasilidades/datasBr.py
+++ python-brasilidades/datasBr.py
@@ -1,5 +1,31 @@
 from datetime import datetime, timedelta
+
+
 class DatasBr:
 
     def __init__(self):
         self.momento_cadastro = datetime.today()
+
+    def mes_cadastro(self):
+        meses_do_ano = [
+            'Janeiro', 'Fevereiro', 'Março', 'Abril',
+            'Maio', 'Junho', 'Julho', 'Agosto',
+            'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+        ]
+        return meses_do_ano[self.momento_cadastro.month - 1]
+
+    def __str__(self):
+        return self.format_data()
+
+    def momento_cadastro(self):
+        return self.__str__()
+
+    def dia_semana(self):
+        dias_da_semana = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado', 'Domingo']
+        return dias_da_semana[self.momento_cadastro.weekday()]
+
+    def format_data(self):
+        return self.momento_cadastro.strftime('%d/%m/%Y %H:%M')
+
+    def tempo_cadastro(self):
+        return datetime.today() - (self.momento_cadastro - timedelta(days=30))
--- python-brasilidades/main.py
+++ python-brasilidades/main.py
@@ -1,5 +1,12 @@
-from datasBr import DatasBr
+from acessaCep import BuscaEndereco
 
-usuario_1 = DatasBr()
+cep = '01001000'
+objeto_cep = BuscaEndereco(cep)
+print(objeto_cep.format_cep())
 
-print(usuario_1.momento_cadastro)
+# r = requests.get('https://viacep.com.br/ws/01001000/json/')
+objeto_cep.acessa_via_cep()
+
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+
+print(bairro, cidade, uf)
